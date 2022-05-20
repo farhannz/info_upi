19e647c7f7f4f3d419b3a2877b2b3eb8e4cf8dcc
Farhan Nurzaman <farhannz@upi.edu>
2022-05-20 16:43:16 +0700

    baris pertama
    Baris pertama tanpa overflow

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # info_upi
-Untuk quiz 2 Flutter
+## Quiz 2 Flutter
+
+Farhan Nurzaman   
+1904908
--- lib/Fakultas.dart
+++ lib/Fakultas.dart
@@ -6,23 +6,64 @@ class Fakultas extends StatelessWidget {
   const Fakultas({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
     return Center(
-      child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
-        InkWell(
-          child: Container(
-            decoration: BoxDecoration(border: Border.all()),
-            padding: EdgeInsets.all(14),
-            child: Text("baris 1"),
+      child: ListView(
+        padding: EdgeInsets.all(20),
+        shrinkWrap: true,
+        children: [
+          InkWell(
+            child: Container(
+              decoration: BoxDecoration(border: Border.all()),
+              padding: EdgeInsets.all(14),
+              child: Wrap(
+                alignment: WrapAlignment.spaceEvenly,
+                spacing: 15,
+                children: [
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "FPMIPA",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        ConstrainedBox(
+                          constraints: BoxConstraints(
+                            maxWidth: .5 * screenWidth,
+                          ),
+                          child: Container(
+                            child: Text(
+                              "Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam",
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Image.network(
+                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                      width: .15 * screenWidth,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            onTap: () {
+              //gunakan navigator untuk panggil RincianFakultas
+            },
           ),
-          onTap: () {
-            //gunakan navigator untuk panggil RincianFakultas
-          },
-        ),
-        Container(
-            decoration: BoxDecoration(border: Border.all()),
-            padding: EdgeInsets.all(14),
-            child: Text("baris kedua")),
-      ]),
+          Container(
+              decoration: BoxDecoration(border: Border.all()),
+              padding: EdgeInsets.all(14),
+              child: Text("baris kedua")),
+        ],
+      ),
     );
   }
 }
